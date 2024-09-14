965508ab954ae1afbd8055592fa963ed0f922c15
Abhishek <asharma991@outlook.com>
2024-09-14 22:51:59 +0530

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,10 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
     base: "/lift-simulation-v1/",
+    build: {
+        outDir: "docs",
+    },
+    server: {
+        open: true,
+    },
 });
